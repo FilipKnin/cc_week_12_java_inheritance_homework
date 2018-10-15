a53e141e68badbf93de884801e5283a5983333aa
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-15 21:04:37 +0100

    Initial commit. Employee class created

---
+++ src/main/java/staff/Employee.java
@@ -0,0 +1,35 @@
+package staff;
+
+public abstract class Employee {
+
+    private String name;
+    private String insuranceNumber;
+    private double salary;
+
+    public Employee(String name, String insuranceNumber, double salary) {
+        this.name = name;
+        this.insuranceNumber = insuranceNumber;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getInsuranceNumber() {
+        return this.insuranceNumber;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void raiseSalary(double amount) {
+        double raisedSalary = this.salary + amount;
+        this.salary = raisedSalary;
+    }
+
+    public double payBonus(){
+        return this.salary * 0.01;
+    }
+}
